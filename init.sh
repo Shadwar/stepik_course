1552bf12f3926dd4c6e18086b4222d75841124e0
Ivan <io@deppa.ru>
2018-01-08 17:25:16 +0300

    asd

--- init.sh
+++ init.sh
@@ -3,4 +3,7 @@ sudo /etc/init.d/nginx restart
 
 source web/myvenv/bin/activate
 gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web hello:hello &
-gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ask ask.wsgi:application &
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application &
+
+# virtualenv --python=/usr/bin/python3.4 web/myvenv
+# source web/myvenv/bin/activate
