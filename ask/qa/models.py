af5104c4950e18f9c09bfbee06b6d6303a13e08b
Ivan <io@deppa.ru>
2018-01-09 22:41:54 +0300

    333

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,16 +13,16 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=1000)
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    likes = models.ManyToManyField(User, related_name='qst')
+    likes = models.ManyToManyField(User, related_name='question_likes')
     objects = QuestionManager()
 
 
 class Answer(models.Model):
     text = models.CharField(max_length=100)
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
